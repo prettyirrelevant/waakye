69b4a58366a87a88b49160231795696629db5c4a
Isaac Adewumi <ienioladewumi@gmail.com>
2023-04-22 11:26:25 +0100

    implemented in-memory cache to reduce auth requests

---
+++ pkg/utils/cache/cache.go
@@ -0,0 +1,47 @@
+package cache
+
+import (
+	"sync"
+	"time"
+)
+
+// GlobalCache is an in-memory cache.
+var GlobalCache = NewCache()
+
+type Cache struct {
+	sync.RWMutex
+	values map[string]cacheItem
+}
+
+type cacheItem struct {
+	value      interface{}
+	expiration time.Time
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		values: make(map[string]cacheItem),
+	}
+}
+
+func (c *Cache) Get(key string) (interface{}, bool) {
+	c.RLock()
+	defer c.RUnlock()
+
+	item, ok := c.values[key]
+	if !ok || item.expiration.Before(time.Now()) {
+		return nil, false
+	}
+	return item.value, true
+}
+
+func (c *Cache) Set(key string, value interface{}, duration time.Duration) {
+	c.Lock()
+	defer c.Unlock()
+
+	expiration := time.Now().Add(duration)
+	c.values[key] = cacheItem{
+		value:      value,
+		expiration: expiration,
+	}
+}
