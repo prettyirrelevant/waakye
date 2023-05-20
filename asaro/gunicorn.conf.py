6b09dbc6943283316a4347ac4d420d5dcf21e4d6
Isaac Adewumi <ienioladewumi@gmail.com>
2023-05-21 00:51:54 +0100

    add gunicorn configuration

---
+++ asaro/gunicorn.conf.py
@@ -0,0 +1,8 @@
+import multiprocessing
+import os
+
+bind = f"0.0.0.0:{os.getenv('PORT', 5001)}"
+worker = multiprocessing.cpu_count() * 2 + 1
+log_file = "-"
+max_requests = 5000
+max_requests_jitter = 50
